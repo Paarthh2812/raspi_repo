95f9c9da6fdb38a935d664a8c9e55411e63d3b1d
Paarthh2812 <paarthh20@gmail.com>
2023-06-29 10:28:57 +0000

    Battery Low Message Completed

--- raspicam_node/src/perception_publisher.py
+++ raspicam_node/src/perception_publisher.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Image,CompressedImage
 import cv2
 from cv_bridge import CvBridge,CvBridgeError
 import numpy as np
+import subprocess
 
 cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
 print(cap.isOpened())
@@ -19,8 +20,13 @@ def main():
     # pub = rospy.Publisher("/image",Image,queue_size=1)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        val = subprocess.check_output(["vcgencmd get_throttled"],shell=True)
+        if list(val)[-2]==48:
+            print("battery at optimal level")
+        else:
+            print("low voltage detected")
         ret, frame = cap.read()
-        print(np.shape(frame))
+        # print(np.shape(frame))
         # msg2 = bridge.cv2_to_imgmsg(frame,encoding="bgr8")
         # pub.publish(msg2)
         msg = bridge.cv2_to_compressed_imgmsg(frame)
